44fc7c8575f030ffc578f2742fcf8c37ee9d77c1
Owen Queen <owencqueen@hotmail.com>
2021-03-01 23:46:00 -0500

    Future formatting options added

--- cs340_project/preprocessing/urls.py
+++ cs340_project/preprocessing/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='preprocessing_home'),
-    path('about/', views.about, name='preprocessing_about'),
+    path('', views.index, name='preprocessing_index'),
+    path('about/', views.about, name='preprocessing_about')
 ]
--- cs340_project/preprocessing/views.py
+++ cs340_project/preprocessing/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def home(request):
-  return render(request, 'preprocessing/home.html')
+def index(request):
+  return render(request, 'preprocessing/index.html')
 
 def about(request):
   return render(request, 'preprocessing/about.html')
