7a46aa291f036e5f1d759d2ddd760992e070da41
Owen Queen <owencqueen@hotmail.com>
2021-03-14 03:17:14 -0400

    File uploading for images done

--- cs340_project/preprocessing/views.py
+++ cs340_project/preprocessing/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.core.files.storage import FileSystemStorage
+
+from .forms import ImagesForm
+from .models import Images
 
 def index(request):
   return render(request, 'preprocessing/index.html')
@@ -8,7 +12,31 @@ def about(request):
   return render(request, 'preprocessing/about.html')
 
 def image(request):
-  return render(request, 'preprocessing/image.html')
+
+  # Allows us to send data back to the page
+  context = {}
+
+  # Image loading code:
+  if request.method == "POST":
+    form = ImagesForm(request.POST, request.FILES)
+    if form.is_valid():
+      form.save()
+      
+  else: # If not a post request, instantiate empty form
+    form = ImagesForm()
+    
+  #  uploaded_file = request.FILES['img_data']
+  #  fs = FileSystemStorage()
+  #  name = fs.save(uploaded_file.name, uploaded_file)
+  #  context['url'] = fs.url(name)
+
+  imgs = Images.objects.all()
+  context['images'] = imgs
+
+  # Get form
+  context['form'] = form
+
+  return render(request, 'preprocessing/image.html', context)
 
 def text(request):
   return render(request, 'preprocessing/text.html')
