5a5efb6bfb3fd2ad54087ffcca795fb0a57096dd
owencqueen <owencqueen@hotmail.com>
2021-04-19 10:39:51 -0400

    Reporting, separate page loading added

--- cs340_project/preprocessing/views.py
+++ cs340_project/preprocessing/views.py
@@ -9,6 +9,7 @@ from .models import Images, Text
 import pandas as pd
 import sys; sys.path.append('media')
 import os
+import json
 
 def index(request):
   return render(request, 'preprocessing/index.html')
@@ -52,21 +53,33 @@ def text(request):
   # Allows us to send data back to the HTML page
   context = {}
 
-  # Image loading code:
-  if request.method == "POST":
-    form = TextForm(request.POST, request.FILES)
-    if form.is_valid():
-      form.save()
+  # # Image loading code:
+  # if request.method == "POST":
+  #   form = TextForm(request.POST, request.FILES)
+  #   if form.is_valid():
+  #     form.save()
       
-  else: # If not a post request, instantiate empty form
-    form = TextForm()
+  # else: # If not a post request, instantiate empty form
+  #   form = TextForm()
 
   txts = Text.objects.all()
   context['texts'] = txts
 
   # Get form
-  context['form'] = form
+  # context['form'] = form
   return render(request, 'preprocessing/text.html', context)
+
+def upload_text(request):
+  # Image loading code:
+  if request.method == "POST":
+    form = TextForm(request.POST, request.FILES)
+    if form.is_valid():
+      form.save()
+      return redirect('preprocessing-text')  
+  else:
+    form = TextForm()
+  return render(request, 'preprocessing/upload_text.html', {'form':form})
+
 
 def delete_text(request, pk):
   '''Allows you to delete text from database'''
@@ -98,25 +111,22 @@ def process_text(request):
 
   print('HELLO')
 
-  fs = Text.objects.values('txt')[0]['txt']
+  # fs = Text.objects.values('txt')[0]['txt']
 
-  file_list = list(Text.objects.values('txt'))
-  for f in file_list:
-    temp_df = pd.read_csv(os.path.join('media', f['txt']))
-    Text.objects.update_or_create(
-      txt = f['txt'], 
-      rows = temp_df.shape[0], 
-      cols = temp_df.shape[1]
-    )
-  
-  print(fs)
-  print(type(fs))
+  # file_list = list(Text.objects.values('txt'))
+  # for f in file_list:
+  #   temp_df = pd.read_csv(os.path.join('media', f['txt']))
+  #   Text.objects.update_or_create(
+  #     txt = f['txt'], 
+  #     rows = temp_df.shape[0], 
+  #     cols = temp_df.shape[1]
+  #   )
 
-  #sys.path.append('media')
-  df = pd.read_csv(os.path.join('media', fs))
-  #df = pd.read_csv(fs)
-  #df = pd.read_csv('../media/text/upec_meta.csv')
-  print(df.shape)
+  # #sys.path.append('media')
+  # df = pd.read_csv(os.path.join('media', fs))
+  # #df = pd.read_csv(fs)
+  # #df = pd.read_csv('../media/text/upec_meta.csv')
+  # print(df.shape)
 
   # Get form
   #context['form'] = form
@@ -129,3 +139,30 @@ def process_text(request):
 def process_image(request):
   # Processing image data (i.e. user interaction)
   return render(request, 'preprocessing/process_image.html')
+
+def generate_report(request, pk):
+  '''Generates report for a given model'''
+  context = {}
+
+  if request.method == 'POST':
+    # Retrieves the requested file:
+    f = Text.objects.get(pk=pk)
+
+    #Computes report for dataframe:
+    df = pd.read_csv(f.filename())
+    report = df.describe().T
+    report['Column Name'] = report.index
+    cols = report.columns.tolist()
+    cols = cols[-1:] + cols[:-1]
+    report = report[cols]
+
+    json_records = report.reset_index().to_json(orient = 'records')
+    data = []
+    data = json.loads(json_records)
+    #context['report'] = data
+    context['report'] = report
+    context['columns'] = [str(c) for c in report.columns]
+
+    context['name'] = f.title
+
+  return render(request, 'preprocessing/report.html', context)
